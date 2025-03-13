5950977adea4753070b4fb6f8a576fc2d645e8cf
sergiohisto <slarrieta02@gmail.comy>
2025-03-13 19:20:29 +0100

    confi de la conexion de la base de datos mysql serverless

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
+import { conn } from "@/libs/mysql";
 
-export function GET() {
-  return NextResponse.json({ message: "holamundo" });
+export async function GET() {
+  const result = await conn.query("SELECT NOW()");
+  console.log(result);
+  return NextResponse.json({ message: result[0]["NOW()"] });
 }
