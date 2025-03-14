2c68e3802593ba28b5b01e6426768eb5477bd91b
sergiohisto <slarrieta02@gmail.comy>
2025-03-14 18:50:57 +0100

    add confi para listar clientes y eliminar clientes desde la api en la base de datos, posibles errores

--- src/app/api/customers/[id]/route.js
+++ src/app/api/customers/[id]/route.js
@@ -1,11 +1,42 @@
+import { conn } from "@/libs/mysql";
 import { NextResponse } from "next/server";
 
-export function GET() {
-  return NextResponse.json("obteniendo clientes");
+export async function GET(request, { params }) {
+  try {
+    const result = await conn.query("SELECT * FROM clientes WHERE id = ?", [
+      params.id,
+    ]);
+    if (result.length === 0) {
+      return NextResponse.json(
+        {
+          message: "cliente no encontrado",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return NextResponse.json(result[0]);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
-export function DELETE() {
-    return NextResponse.json("Eliminando clientes");
-  }
-  export function PUT() {
-    return NextResponse.json("Actualizando clientes");
-  }
+export async function DELETE(request, { params }) {
+  const result = await conn.query("DELETE FROM clientes WHERE id = ?", [
+    params.id,
+  ]);
+  return new Response(null, {
+    status: 204,
+  });
+}
+export function PUT() {
+  return NextResponse.json("Actualizando clientes");
+}
--- src/app/api/customers/route.js
+++ src/app/api/customers/route.js
@@ -1,8 +1,21 @@
 import { conn } from "@/libs/mysql";
 import { NextResponse } from "next/server";
 
-export function GET() {
-  return NextResponse.json("lista de clientes");
+export async function GET() {
+  try {
+    const results = await conn.query("SELECT * FROM clientes");
+    return NextResponse.json(results);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
 export async function POST(request) {
   try {
@@ -30,7 +43,18 @@ export async function POST(request) {
       fecha_fin_membresia,
     });
 
-    console.log(result);
+    return NextResponse.json({
+      nombre,
+      apellido,
+      email,
+      telefono,
+      fecha_registro,
+      estado,
+      membresia_tipo,
+      fecha_inicio_membresia,
+      fecha_fin_membresia,
+      id: result.insertId,
+    });
 
     return NextResponse.json("Creando clientes");
   } catch (error) {
