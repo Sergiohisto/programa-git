329695229e02e4bf024f928827d290e251801c06
sergiohisto <slarrieta02@gmail.comy>
2025-06-30 11:41:42 +0200

    ultima actualizacion

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,17 @@
 import { About } from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Work from "@/components/Work";
 
 const page = () => {
   return (
     <section>
-     <header>
-       <Header />
-     </header>
+      <header>
+        <Header />
+      </header>
       <Hero />
       <About />
+      <Work />
     </section>
   );
 };
---
+++ src/components/Work.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Work = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="flex flex-col">
+            <p>ORION GYM</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis quaerat officiis accusantium magnam unde.</p>
+        </div>
+        <div>CARD</div>
+      </div>
+    </section>
+  );
+};
+
+export default Work;
